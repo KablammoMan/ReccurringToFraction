8144d0cf6b3f29d85f2641c661409570dbb31d99
KablammoMan <codernaffilio@gmail.com>
2023-08-15 10:39:34 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import math
 
+def euclid(n, d):
+    if n % d == 0:
+        return d
+    else:
+        return euclid(d, n%d)
+
 
 def simplify(n, d):
-    nfacts = []
-    dfacts = []
-    hcf = 1
-    for i in range(1, math.floor(math.sqrt(abs(n)))+1):
-        if n % i == 0:
-            nfacts.append(i)
-            nfacts.append(n / i)
-    for i in range(1, math.floor(math.sqrt(abs(d)))+1):
-        if d % i == 0:
-            dfacts.append(i)
-            dfacts.append(d / i)
-    for i in nfacts:
-        if i in dfacts and i > hcf:
-            hcf = i
+    factors = 0
+    while n > d:
+        factors += 1
+        n -= d
+    hcf = euclid(n,d)
     new_n = int(n / hcf)
     new_d = int(d / hcf)
-    return f"{new_n}/{new_d}"
+    return f"{factors} {new_n}/{new_d}"
 
 
 num = input("Number: ")
 
 rec = int(input("Length: "))
+recp = num[(len(num)-rec):]
 
-
-fy = 10 ** (len(num.split(".")[1]) - rec)
-fx = fy * float(num) * 100
+dif = len(num.split(".")[1]) - rec
+fy = 10 ** dif
+fx = fy * float(num)
 print(f"{fy}x = {fx}")
 ly = fy * (10 ** rec)
 lx = fx * (10 ** rec)
-print(f"{ly}x = {lx}")
+print(f"{ly}x = {int(lx)}.{recp}")
 n = int(lx - int(fx))
 d = int(ly - fy)
 print(f"x = {n}/{d}")
